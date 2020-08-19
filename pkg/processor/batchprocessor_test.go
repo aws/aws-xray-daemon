daae766e0fe65216eaba3c75f3f351e9cd6086f3
Lei Wang <wangzl@amazon.com>
2020-08-18 22:34:36 -0700

    fix unit test

--- pkg/processor/batchprocessor_test.go
+++ pkg/processor/batchprocessor_test.go
@@ -97,7 +97,7 @@ func TestPollSendSuccess(t *testing.T) {
 		xRay:    xRay,
 		done:    make(chan bool),
 	}
-	testMessage := "Test Message"
+	testMessage := "{\"id\":\"9472\""
 	batch := []*string{&testMessage}
 	s.send(batch)
 
@@ -271,7 +271,7 @@ func TestPutTraceSegmentsParameters(t *testing.T) {
 		xRay:    xRay,
 		done:    make(chan bool),
 	}
-	testMessage := "Test Message"
+	testMessage := "{\"id\":\"9472\""
 	batch := []*string{&testMessage}
 	s.send(batch)
 
