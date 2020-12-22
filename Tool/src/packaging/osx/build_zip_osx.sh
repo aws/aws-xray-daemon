5d4c5ab16b9af1d549e3191c367523fb9e505a00
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-22 14:26:49 +0900

    Tweak artifact build and wire to workflow (#84)
    * Build to dist folder and add arm64
    
    * Tweak and wire to workflow
    
    * Use ldflags to set version instead of script.
    
    * Fix runner.os check and only zip on Linux since not part of development.
    
    * Upload distributions to artifact.
    
    * Linux
    
    * Remove VERSION in favor of CI env variable and git tags.
    
    * Cleanups

--- Tool/src/packaging/osx/build_zip_osx.sh
+++ Tool/src/packaging/osx/build_zip_osx.sh
@@ -3,11 +3,7 @@ echo "****************************************"
 echo "Creating zip file for OS-X amd64"
 echo "****************************************"
 
-BUILD_FOLDER=${BGO_SPACE}/build/xray
+DIST_FOLDER=${BGO_SPACE}/build/dist/
+cd $DIST_FOLDER
 
-if [ -f ${BUILD_FOLDER}/xray-osx.zip ]
-then
-    rm ${BUILD_FOLDER}/xray-osx.zip
-fi
-cd ${BUILD_FOLDER}
-zip aws-xray-daemon-macos-`cat ${BGO_SPACE}/VERSION`.zip xray_mac cfg.yaml LICENSE THIRD-PARTY-LICENSES.txt
+zip aws-xray-daemon-macos-amd64-${VERSION}.zip ../xray-mac-amd64/xray ../xray/cfg.yaml ../xray/LICENSE ../xray/THIRD-PARTY-LICENSES.txt
