71213c86c8a0694f10ecdea818d451c38047c4dc
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-01-13 13:00:18 +0900

    trailing slash

--- Tool/src/build-in-docker.sh
+++ Tool/src/build-in-docker.sh
@@ -5,7 +5,7 @@ BUILD_FOLDER=${TARGETPLATFORM/\//-}
 
 if [[ -d "/workspace/build/xray-${BUILD_FOLDER}" ]]; then
   echo "Copying prebuilt binary"
-  cp /workspace/build/xray-${BUILD_FOLDER}/xray /workspace
+  cp /workspace/build/xray-${BUILD_FOLDER}/xray /workspace/
 else
   echo "Building from source"
   CGO_ENABLED=0 GOOS=linux GOARCH=$(echo $TARGETPLATFORM | cut -d'/' -f2) go build -a -ldflags '-extldflags "-static"' -o /workspace/xray /workspace/cmd/tracing
