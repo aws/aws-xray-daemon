07f61996271dde468c26affcb04bd12188b4c751
Prashant Srivastava <50466688+srprash@users.noreply.github.com>
2021-01-21 00:33:12 -0800

    Packaging daemon into amd64 and arm64 type rpm (#91)
    * Passing in the VERSION env var to the packaging job
    
    * Packaging arm64 and amd64 type rpms
    
    * using --target instead of setarch for building rpm

--- Tool/src/packaging/linux/build_rpm_linux.sh
+++ Tool/src/packaging/linux/build_rpm_linux.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/env bash
+
+set -e
+
+ARCH=$1
+if [[ $ARCH == "amd64" ]]
+then
+    BUILD_ARCH=x86_64
+elif [[ $ARCH == "arm64" ]]
+then
+    BUILD_ARCH=aarch64
+else
+    echo "Invalid architecture input. Exiting"
+    exit 1
+fi
+
 echo "*************************************************"
-echo "Creating rpm file for Amazon Linux and RHEL amd64"
+echo "Creating rpm file for Amazon Linux and RHEL ${ARCH}"
 echo "*************************************************"
 
-rm -rf ${BGO_SPACE}/bin/linux_amd64/linux
+rm -rf ${BGO_SPACE}/bin/linux_${ARCH}/linux
 
 echo "Creating rpmbuild workspace"
-mkdir -p ${BGO_SPACE}/bin/linux_amd64/linux/rpmbuild/{RPMS,SRPMS,BUILD,COORD_SOURCES,SPECS,DATA_SOURCES}
-mkdir -p ${BGO_SPACE}/bin/linux_amd64/linux/usr/bin/
-mkdir -p ${BGO_SPACE}/bin/linux_amd64/linux/etc/amazon/xray/
-mkdir -p ${BGO_SPACE}/bin/linux_amd64/linux/etc/init/
-mkdir -p ${BGO_SPACE}/bin/linux_amd64/linux/etc/systemd/system/
+mkdir -p ${BGO_SPACE}/bin/linux_${ARCH}/linux/rpmbuild/{RPMS,SRPMS,BUILD,COORD_SOURCES,SPECS,DATA_SOURCES}
+mkdir -p ${BGO_SPACE}/bin/linux_${ARCH}/linux/usr/bin/
+mkdir -p ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/amazon/xray/
+mkdir -p ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/init/
+mkdir -p ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/systemd/system/
 
 echo "Copying application files"
-cp ${BGO_SPACE}/build/xray-linux-amd64/xray ${BGO_SPACE}/bin/linux_amd64/linux/usr/bin/
-cp ${BGO_SPACE}/pkg/cfg.yaml ${BGO_SPACE}/bin/linux_amd64/linux/etc/amazon/xray/cfg.yaml
-cp ${BGO_SPACE}/Tool/src/packaging/linux/xray.conf ${BGO_SPACE}/bin/linux_amd64/linux/etc/init/
-cp ${BGO_SPACE}/Tool/src/packaging/linux/xray.service ${BGO_SPACE}/bin/linux_amd64/linux/etc/systemd/system/
-cp ${BGO_SPACE}/LICENSE ${BGO_SPACE}/bin/linux_amd64/linux/etc/amazon/xray/
-cp ${BGO_SPACE}/THIRD-PARTY-LICENSES.txt ${BGO_SPACE}/bin/linux_amd64/linux/etc/amazon/xray/
+cp ${BGO_SPACE}/build/xray-linux-${ARCH}/xray ${BGO_SPACE}/bin/linux_${ARCH}/linux/usr/bin/
+cp ${BGO_SPACE}/pkg/cfg.yaml ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/amazon/xray/cfg.yaml
+cp ${BGO_SPACE}/Tool/src/packaging/linux/xray.conf ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/init/
+cp ${BGO_SPACE}/Tool/src/packaging/linux/xray.service ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/systemd/system/
+cp ${BGO_SPACE}/LICENSE ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/amazon/xray/
+cp ${BGO_SPACE}/THIRD-PARTY-LICENSES.txt ${BGO_SPACE}/bin/linux_${ARCH}/linux/etc/amazon/xray/
 
 echo "Creating the rpm package"
 SPEC_FILE="${BGO_SPACE}/Tool/src/packaging/linux/xray.spec"
-BUILD_ROOT="${BGO_SPACE}/bin/linux_amd64/linux"
-setarch x86_64 rpmbuild --define "rpmversion ${VERSION}" --define "_topdir bin/linux_amd64/linux/rpmbuild" -bb --buildroot ${BUILD_ROOT} ${SPEC_FILE}
+BUILD_ROOT="${BGO_SPACE}/bin/linux_${ARCH}/linux"
+rpmbuild --target ${BUILD_ARCH}-linux --define "rpmversion ${VERSION}" --define "_topdir bin/linux_${ARCH}/linux/rpmbuild" -bb --buildroot ${BUILD_ROOT} ${SPEC_FILE}
 
 echo "Copying rpm files to bin"
-cp ${BGO_SPACE}/bin/linux_amd64/linux/rpmbuild/RPMS/x86_64/*.rpm ${BGO_SPACE}/build/dist/aws-xray-daemon-linux-amd64-${VERSION}.rpm
+cp ${BGO_SPACE}/bin/linux_${ARCH}/linux/rpmbuild/RPMS/${BUILD_ARCH}/*.rpm ${BGO_SPACE}/build/dist/aws-xray-daemon-linux-${ARCH}-${VERSION}.rpm
