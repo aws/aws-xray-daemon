b003d4e050a579ab7ea4c0ca744d10a804c88850
Prashant Srivastava <srprash@amazon.com>
2021-01-25 13:16:42 -0800

    renaming instead of rebuilding

--- Tool/src/packaging/build-package-legacy.sh
+++ Tool/src/packaging/build-package-legacy.sh
@@ -16,18 +16,19 @@ cp ${BGO_SPACE}/build/dist/aws-xray-daemon-linux-arm64-${VERSION}.rpm ${BGO_SPAC
 cp ${BGO_SPACE}/build/dist/aws-xray-daemon-linux-arm64-${VERSION}.deb ${BGO_SPACE}/build/dist/aws-xray-daemon-arm64-3.x.deb
 
 echo "Building and packaging legacy artifacts for MacOS"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o ${BGO_SPACE}/build/xray-mac-legacy/xray_mac ${BGO_SPACE}/cmd/tracing/daemon.go ${BGO_SPACE}/cmd/tracing/tracing.go
-cp ${BGO_SPACE}/build/xray-mac-legacy/xray_mac xray_mac
+unzip -q -o aws-xray-daemon-macos-amd64-${VERSION}.zip
+mv xray xray_mac
 zip aws-xray-daemon-macos-3.x.zip xray_mac cfg.yaml LICENSE THIRD-PARTY-LICENSES.txt
 rm xray_mac
 
 echo "Building and packaging legacy artifacts for Windows"
-GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o ${BGO_SPACE}/build/xray-win-legacy/xray.exe ${BGO_SPACE}/cmd/tracing/daemon.go ${BGO_SPACE}/cmd/tracing/tracing_windows.go
-GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o ${BGO_SPACE}/build/xray-win-legacy/xray_windows.exe ${BGO_SPACE}/cmd/tracing/daemon.go ${BGO_SPACE}/cmd/tracing/tracing.go
-cp ${BGO_SPACE}/build/xray-win-legacy/xray.exe xray.exe
+unzip -q -o aws-xray-daemon-windows-amd64-service-${VERSION}.zip
+mv xray_service.exe xray.exe
 zip aws-xray-daemon-windows-service-3.x.zip xray.exe cfg.yaml LICENSE THIRD-PARTY-LICENSES.txt
 rm xray.exe
-cp ${BGO_SPACE}/build/xray-win-legacy/xray_windows.exe xray_windows.exe
+
+unzip -q -o aws-xray-daemon-windows-amd64-${VERSION}.zip
+mv xray.exe xray_windows.exe
 zip aws-xray-daemon-windows-process-3.x.zip xray_windows.exe cfg.yaml LICENSE THIRD-PARTY-LICENSES.txt
 rm xray_windows.exe
 
