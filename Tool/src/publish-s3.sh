1eb7a674cd337c9f671063a538e9d4de8d62bf66
bhautikpip <bhautip@amazon.com>
2021-05-04 00:50:07 -0700

    removed testing code

--- Tool/src/publish-s3.sh
+++ Tool/src/publish-s3.sh
@@ -1,17 +1,3 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is located at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# or in the "license" file accompanying this file. This file is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-# express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
-
-
 #! /bin/bash
 
 set -e
@@ -28,7 +14,7 @@ set -e
 
 # check environment vars
 if [ -z ${s3_bucket_name} ]; then
-    s3_bucket_name="aws-xray-daemon-assets.staging"
+    s3_bucket_name="aws-xray-assets.staging"
 	echo "use default s3_bucket_name: ${s3_bucket_name}"
 else
 	echo "load s3_bucket_name from env var: ${s3_bucket_name}"
@@ -85,6 +71,7 @@ declare -a required_files=(
   "upload_ready_binaries/${package_name}-windows-service-${major_version}.zip.sig"
 )
 
+# check required files are available
 for i in "${required_files[@]}"
 do
 	if [ ! -f "${i}" ]; then
@@ -93,7 +80,7 @@ do
 	fi
 done
 
-# upload packages to s3
+# upload daemon binaries to s3
 for i in "${required_files[@]}"
 do
 	aws s3api put-object --bucket "${s3_bucket_name}" --key "${release_version}/${i}" --body "${i}"
